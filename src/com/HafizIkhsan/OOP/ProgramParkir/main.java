ed15476f3e46ccf630108c6140b5987fb44d4512
HafizIkhsan <hafizikhsan73@gmail.com>
2024-03-10 05:53:29 +0700

    first commit

---
+++ src/com/HafizIkhsan/OOP/ProgramParkir/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.HafizIkhsan.OOP.ProgramParkir;
+
+/**
+ *
+ * @author HafizIkhsan
+ */
+import java.util.Scanner;
+
+public class main {
+    
+}
